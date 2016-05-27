e9bd199bf3da3b4f6ccd7cbc8735a2da49b9b229
Daniel <Daniel@iMac-Daniel.local>
2016-05-27 14:06:10 +0300

    Add hometask files 86b,322,323

---
+++ src/com/softserve/edu/Excercise86B.java
@@ -0,0 +1,17 @@
+
+package naturalnambers;
+
+/**
+ *
+ * Dmitriy Voropai
+ */
+public class Excercise86B {
+    public static void main(String[] args) {
+        System.out.println(NaturalN(10));
+    }
+    public static int NaturalN(int n){
+        if (n==0 || n<0) {return 1;}
+        if (n==1) {return 1;}
+        else {return n*(n+1)/2;}
+    }
+}
---
+++ src/com/softserve/edu/Exercise322.java
@@ -0,0 +1,27 @@
+
+package naturalnambers.excersice322;
+
+public class Exercise322 {
+    public static void main(String[] args) {
+       int [] divisors = new int [10001];
+             int MaxSumValue = 0;
+               int b = 0;
+        for (int n = 1; n <= divisors.length-1; n++) { 
+            int sum=1;
+            for (int i = 2; i <=n ; i++) {
+                 if (n%i==0) {
+               sum+=i;}  
+            }
+            divisors[n] = sum;
+                for(int k = 0; k<divisors.length; k++){
+                    if(MaxSumValue < divisors[n])
+                    MaxSumValue = divisors[n]; 
+              }
+                
+                for(int z=0; z<divisors.length; z++) 
+                         if(divisors[z] == MaxSumValue){
+                         b=z;             
+                         }
+        }      System.out.println("Value: " +b+  "   Max sum of divisors is:   " +MaxSumValue);
+    }
+}
