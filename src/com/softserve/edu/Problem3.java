e8055e8ca6f24eef3d2f6b2af49c30ac44a98aca
Володимир <>
2016-05-27 15:48:49 +0300

    refactor Problem 3

--- src/com/softserve/edu/Problem3.java
+++ src/com/softserve/edu/Problem3.java
@@ -5,48 +5,37 @@ import java.util.ArrayList;
 public class Problem3 {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("2 digits Armstrong numbers");
-		for(int i = 10; i<=99; i++) {
-			ArrayList<Integer> list = new ArrayList<Integer>();
-			int n = i;
-			while (n >= 10) {
-				int k = n % 10;
-				list.add(k);
-				n = (n - n%10)/10;
-			}
-			list.add(n);
-			if (Math.pow(list.get(0), 2) + Math.pow(list.get(1), 2) == i) System.out.println(i);
-			list.clear();
+		for (int i = 10; i <= 99; i++) {
+			ArrayList<Integer> k = ll(i);
+			if (Math.pow(k.get(0), 2) + Math.pow(k.get(1), 2) == i)
+				System.out.println(i);
 		}
-		
+
 		System.out.println("3 digits Armstrong numbers");
-		for(int i = 100; i<=999; i++) {
-			ArrayList<Integer> list = new ArrayList<Integer>();
-			int n = i;
-			while (n >= 10) {
-				int k = n % 10;
-				list.add(k);
-				n = (n - n%10)/10;
-			}
-			list.add(n);
-			if (Math.pow(list.get(0), 3) + Math.pow(list.get(1), 3) + Math.pow(list.get(2), 3) == i) System.out.println(i);
-			list.clear();
+		for (int i = 100; i <= 999; i++) {
+			ArrayList<Integer> k = ll(i);
+			if (Math.pow(k.get(0), 3) + Math.pow(k.get(1), 3) + Math.pow(k.get(2), 3) == i)
+				System.out.println(i);
 		}
-		
+
 		System.out.println("3 digits Armstrong numbers");
-		for(int i = 1000; i<=9999; i++) {
-			ArrayList<Integer> list = new ArrayList<Integer>();
-			int n = i;
-			while (n >= 10) {
-				int k = n % 10;
-				list.add(k);
-				n = (n - n%10)/10;
-			}
-			list.add(n);
-			if (Math.pow(list.get(0), 4) + Math.pow(list.get(1), 4) + Math.pow(list.get(2), 4) + Math.pow(list.get(3), 4)== i) System.out.println(i);
-			list.clear();
+		for (int i = 1000; i <= 9999; i++) {
+			ArrayList<Integer> k = ll(i);
+			if (Math.pow(k.get(0), 4) + Math.pow(k.get(1), 4) + Math.pow(k.get(2), 4) + Math.pow(k.get(3), 4) == i)
+				System.out.println(i);
 		}
 	}
 
+	public static ArrayList<Integer> ll(int n) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		while (n >= 10) {
+			int k = n % 10;
+			list.add(k);
+			n = (n - n % 10) / 10;
+		}
+		list.add(n);
+		return list;
+	}
 }
