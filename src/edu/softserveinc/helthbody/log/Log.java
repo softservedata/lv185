ee1f2513c85b649f962f7001e2a412d21106f64e
Володимир <>
2016-06-13 00:29:30 +0300

    Add log4j

---
+++ src/edu/softserveinc/helthbody/log/Log.java
@@ -0,0 +1,17 @@
+package edu.softserveinc.helthbody.log;
+
+import java.io.InputStream;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Log {
+    
+    public static Logger init(String className) {
+    	ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+    	InputStream is = classloader.getResourceAsStream("log4j.properties");
+        final Logger logger = Logger.getLogger(className);
+        PropertyConfigurator.configure(is);
+        return logger;
+    }
+}
