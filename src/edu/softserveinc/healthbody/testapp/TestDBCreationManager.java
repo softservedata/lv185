935e2cc8d896ca35c5a192f80131173d9ef742c2
Володимир <>
2016-06-14 00:28:01 +0300

    Add log to TestDBCreationManager

--- src/edu/softserveinc/healthbody/testapp/TestDBCreationManager.java
+++ src/edu/softserveinc/healthbody/testapp/TestDBCreationManager.java
@@ -5,24 +5,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.apache.log4j.Logger;
+
 import edu.softserveinc.healthbody.db.DBCreationManager;
 import edu.softserveinc.healthbody.db.DBCreationManager.TableQueries;
+import edu.softserveinc.healthbody.log.Log;
 
 public class TestDBCreationManager {
 
+	private static Logger logger = Log.init(TestDBCreationManager.class.getName());
 	private static Connection con = null;
 	private static String username = "postgres";
 	private static String password = "root";
-
 	private static String URL = "jdbc:postgresql://localhost:5432/";
 	private static String databaseName = "healthbodydb";
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		System.out.println("Start...");
+		logger.info("TestDBCreationManager starts...");
 		DriverManager.registerDriver(new org.postgresql.Driver());
 		con = DriverManager.getConnection(URL, username, password);
 		if (con != null) {
-			System.out.println("Connection Successful! \n");
+			logger.info("Connection Successful! \n");
 		}
 		if (con == null) {
 			System.exit(0);
@@ -31,14 +34,14 @@ public class TestDBCreationManager {
 	
 		try {
 			DBCreationManager.getInstance().createDatabase(st, databaseName);
-			System.out.println("Database - "+databaseName+" was created");
+			logger.info("Database - "+databaseName+" was created");
 		
 			for (TableQueries query : TableQueries.values()) {
-				System.out.println("Creating " + query.name());
+				logger.info("Creating " + query.name());
 				DBCreationManager.getInstance().createTable(st, query.toString());
 			}
 		} catch (SQLException e) {
-			e.printStackTrace();
+			logger.error("Database didn't create", e);
 		}
 
 		if (st != null)
