ac24370d18bf9b0fee1fdd320cb04c8926ae75ef
Anatolii Tsaruk <anatolii.tsaruk@gmail.com>
2016-06-13 15:02:52 +0300

    Logger properties filepath changed.

--- src/edu/softserveinc/helthbody/log/Log.java
+++ src/edu/softserveinc/helthbody/log/Log.java
@@ -6,10 +6,11 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 public class Log {
+	private final static String LOGGER_PROPERTIES_FILENAME = "./resources/log4j.properties";
     
     public static Logger init(String className) {
     	ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-    	InputStream is = classloader.getResourceAsStream("log4j.properties");
+    	InputStream is = classloader.getResourceAsStream(LOGGER_PROPERTIES_FILENAME);
         final Logger logger = Logger.getLogger(className);
         PropertyConfigurator.configure(is);
         return logger;
