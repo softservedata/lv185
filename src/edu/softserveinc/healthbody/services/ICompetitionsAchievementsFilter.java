03dac6c1ba95e74806d13db074d2e024efe60f82
Iryna Kret <irynakret@gmail.com>
2016-06-19 16:22:55 +0300

    Added ICompetitionsAchievements

--- src/edu/softserveinc/healthbody/services/ICompetitionsAchievementsFilter.java
+++ src/edu/softserveinc/healthbody/services/ICompetitionsAchievementsFilter.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import edu.softserveinc.healthbody.dto.UserCompetitionsDTO;
 
-public interface CompetitionsAchievementsFilter {
+public interface ICompetitionsAchievementsFilter {
 	
 	List<UserCompetitionsDTO> getAllCompetitionsAchievements(int partSize, int partNumber,
 			String login, String competition, String userScore, String awardName, 
