9efe8248296cbab2265de0779cab43881473149a
volodya4u <volodya4u@hotmail.com>
2016-07-03 00:44:28 +0300

    Start rewriting UserViewTest

--- src/test/java/edu/softserveinc/healthbody/UserViewTest.java
+++ src/test/java/edu/softserveinc/healthbody/UserViewTest.java
@@ -1,53 +1,147 @@
 package edu.softserveinc.healthbody;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import edu.softserveinc.healthbody.db.DBCreationManager;
+import edu.softserveinc.healthbody.db.DBPopulateManager;
 import edu.softserveinc.healthbody.dto.UserDTO;
+import edu.softserveinc.healthbody.exceptions.DataBaseReadingException;
 import edu.softserveinc.healthbody.exceptions.JDBCDriverException;
+import edu.softserveinc.healthbody.exceptions.QueryNotFoundException;
 import edu.softserveinc.healthbody.exceptions.TransactionException;
 import edu.softserveinc.healthbody.services.impl.UsersViewServiceImpl;
 
 public class UserViewTest {
 	private static Logger logger = LoggerFactory.getLogger(UserViewTest.class.getName());
 	
+	@BeforeSuite
+	public void setUpBeforeSuite() {
+		try (Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "root");
+				Statement st = con.createStatement()){
+			if (!DBCreationManager.getInstance().createDatabase(st, "healthbodydb")){
+				logger.info("Couldn't create database, because encounter some problem!");
+				System.exit(0); 
+			}
+		} catch (SQLException e) {
+			logger.error("Problem with creating database", e);
+			System.exit(0); 
+		}
+		try(Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/healthbodydb", "postgres", "root");
+				Statement st = con.createStatement()){
+			DBCreationManager dbCReationManager = DBCreationManager.getInstance();
+			for (String query : dbCReationManager.getListOfQueries()) {
+				logger.info("Creating " + query.split("\"")[1]);
+				dbCReationManager.createTable(st, query);
+			}
+		} catch (SQLException e) {
+			logger.error("Problem with creating tables data", e);
+			System.exit(0); 
+		}
+	}
+	
+	@AfterSuite
+	public void tearDownAfterSuite() {
+		try (Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "root");
+				Statement st = con.createStatement()){
+				if (!DBCreationManager.getInstance().dropDatabase(st, "healthbodydb")){
+					logger.info("Couldn't delete database, because encounter some problem!");
+					System.exit(0); 
+				}
+				else {
+				logger.error("Database wasn't deleted");			
+			}
+		} catch (SQLException e) {
+			logger.error("Problem with deleting database", e);
+			System.exit(0); 
+		}
+	}
+	
+	@BeforeClass
+	public void setUpBeforeClass() throws JDBCDriverException, QueryNotFoundException, DataBaseReadingException, SQLException {
+		DBPopulateManager.getInstance().populateUsersTable();
+		DBPopulateManager.getInstance().populateGroupsTable();
+		DBPopulateManager.getInstance().populateUserGroupsTable();
+		DBPopulateManager.getInstance().populateAwardsTable();
+		DBPopulateManager.getInstance().populateCompetitionsTable();
+		DBPopulateManager.getInstance().populateCriteriaTable();
+		DBPopulateManager.getInstance().populateGroupCompetitionsTable();
+		DBPopulateManager.getInstance().populateMetaDataTable();
+		DBPopulateManager.getInstance().populateRolesTable();
+		DBPopulateManager.getInstance().populateUserCompetitionsTable();
+		logger.info("Populated All tables");					
+	}
+	
+	@AfterClass
+	public void tearDownAfterClass() throws SQLException, JDBCDriverException {
+		DBPopulateManager.getInstance().deleteAllFromTables();
+	}
+	
 	@Test
-	public void testUserView() {
-
-		///before
-		
-		
+	public void testUserViewGetAll() {
 		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
-
 		try {
 			List<UserDTO> ud1 = uvs.getAll(1, 5);
-			//ud1.size();
-			
-			
-			
-			
-			List<UserDTO> ud2 = uvs.getAll(5, 1);
-			
-			
-			
-//			
-//			List<UserDTO> ud2 = uvs.getAllbyAdmin(1, 5);
-			List<UserDTO> ud3 = uvs.getAllinCompetition(1, 5);
-//			List<UserDTO> ud4 = uvs.getAllinGroup(1, 5);
-//			List<UserDTO> ud5 = uvs.getAlltoAddInCompetition(1, 5);
-//
 			logger.info("In getAll: " + Arrays.toString(ud1.toArray()));
-			logger.info("In getAllbyAdmin: " + Arrays.toString(ud2.toArray()));
-			logger.info("In getAllinCompetition: " + Arrays.toString(ud3.toArray()));
-//			logger.info("In getAllinGroup: " + Arrays.toString(ud4.toArray()));
-//			logger.info("In getAllinGroup: " + Arrays.toString(ud5.toArray()));
 		} catch (JDBCDriverException | SQLException | TransactionException e) {
 			logger.error("GetAll didn't work", e);
 		}
 	}
+	
+	@Test
+	public void testUserViewGetAllbyAdmin() {
+		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
+		try {
+			List<UserDTO> ud2 = uvs.getAllbyAdmin(1, 5);
+			logger.info("In getAllbyAdmin: " + Arrays.toString(ud2.toArray()));
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			logger.error("GetAllbyAdmin didn't work", e);
+		}
+	}
+	
+	@Test
+	public void testUserViewGetAllinCompetition() {
+		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
+		try {
+			List<UserDTO> ud3 = uvs.getAllinCompetition(1, 5);
+			logger.info("In getAllinCompetition: " + Arrays.toString(ud3.toArray()));
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			logger.error("GetAllinCompetition didn't work", e);
+		}
+	}
+	
+	@Test
+	public void testUserViewGetAllinGroup() {
+		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
+		try {
+			List<UserDTO> ud4 = uvs.getAllinGroup(1, 5);
+			logger.info("In getAllinGroup: " + Arrays.toString(ud4.toArray()));
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			logger.error("GetAllinGroup didn't work", e);
+		}
+	}
+	
+	@Test
+	public void testUserViewGetAlltoAddInCompetition() {
+		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
+		try {
+			List<UserDTO> ud5 = uvs.getAlltoAddInCompetition(1, 5);
+			logger.info("In getAlltoAddInCompetition: " + Arrays.toString(ud5.toArray()));
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			logger.error("GetAlltoAddInCompetition didn't work", e);
+		}
+	}
+
 }
