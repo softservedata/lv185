61d3e5a454162ffdc3f9225f05138ce2cee161e1
Володимир <@192.168.1.22>
2016-07-10 23:21:30 +0300

    Rewrite UsersViewServiceImplTest

--- src/test/java/edu/softserveinc/healthbody/UsersViewServiceImplTest.java
+++ src/test/java/edu/softserveinc/healthbody/UsersViewServiceImplTest.java
@@ -2,6 +2,7 @@ package edu.softserveinc.healthbody;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.fail;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -17,55 +18,86 @@ import edu.softserveinc.healthbody.services.impl.UsersViewServiceImpl;
 
 public class UsersViewServiceImplTest {
 	private static Logger logger = LoggerFactory.getLogger(UsersViewServiceImplTest.class.getName());
-	
-	@Test 
-	public void testUserViewGetAll() throws JDBCDriverException, SQLException, TransactionException {
-		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
-		List<UserDTO> ud1 = uvs.getAll(1, 2);
-		logger.info("testUserViewGetAll");
-		logger.info(ud1.toString());
-		assertNotNull(ud1);
-		assertEquals(ud1.size(), 2);
-	}
-	
+	private static final String EXCEPTION_CATCHED = "Exception catched while running test method.";
+
 	@Test
-	public void testUserViewGetAllbyAdmin() throws JDBCDriverException, SQLException, TransactionException {
+	public void testUserViewGetAll() {
 		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
-		List<UserDTO> ud3 = uvs.getAllbyAdmin(1, 2);
-		logger.info("testUserViewGetAllbyAdmin");
-		logger.info(ud3.toString());
-		assertNotNull(ud3);
-		assertEquals(ud3.size(), 2);	
+		List<UserDTO> ud1;
+		try {
+			ud1 = uvs.getAll(1, 2);
+			assertNotNull(ud1);
+			assertEquals(ud1.size(), 2);
+			logger.info("testUserViewGetAll");
+			logger.info(ud1.toString());
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			logger.error(EXCEPTION_CATCHED, e);
+			fail(EXCEPTION_CATCHED, e);
+		}
 	}
-	
+
 	@Test
-	public void testUserViewGetAllinCompetition() throws JDBCDriverException, SQLException, TransactionException {
+	public void testUserViewGetAllbyAdmin() {
 		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
-		List<UserDTO> ud4 = uvs.getAllinCompetition(1, 2);
-		logger.info("testUserViewGetAllinCompetition");
-		logger.info(ud4.toString());
-		assertNotNull(ud4);
-		assertEquals(ud4.size(), 2);	
+		List<UserDTO> ud3;
+		try {
+			ud3 = uvs.getAllbyAdmin(1, 2);
+			assertNotNull(ud3);
+			assertEquals(ud3.size(), 2);
+			logger.info("testUserViewGetAllbyAdmin");
+			logger.info(ud3.toString());
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			logger.error(EXCEPTION_CATCHED, e);
+			fail(EXCEPTION_CATCHED, e);
+		}
 	}
-	
-	@Test 
-	public void testUserViewGetAllinGroup() throws JDBCDriverException, SQLException, TransactionException {
-		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
-		List<UserDTO> ud5 = uvs.getAllinGroup(1, 2);
-		logger.info("testUserViewGetAllinGroup");
-		logger.info(ud5.toString());
-		assertNotNull(ud5);
-		assertEquals(ud5.size(), 2);	
-	}
-	
+
 	@Test
-	public void testUserViewGetAlltoAddInCompetition() throws JDBCDriverException, SQLException, TransactionException {
+	public void testUserViewGetAllinCompetition() {
 		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
-		List<UserDTO> ud6 = uvs.getAlltoAddInCompetition(1, 2);
-		logger.info("testUserViewGetAlltoAddInCompetition");
-		logger.info(ud6.toString());
-		assertNotNull(ud6);
-		assertEquals(ud6.size(), 2);	
+		List<UserDTO> ud4;
+		try {
+			ud4 = uvs.getAllinCompetition(1, 2);
+			assertNotNull(ud4);
+			assertEquals(ud4.size(), 2);
+			logger.info("testUserViewGetAllinCompetition");
+			logger.info(ud4.toString());
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			logger.error(EXCEPTION_CATCHED, e);
+			fail(EXCEPTION_CATCHED, e);
+		}
+	}
+
+	@Test
+	public void testUserViewGetAllinGroup() {
+		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
+		List<UserDTO> ud5;
+		try {
+			ud5 = uvs.getAllinGroup(1, 2);
+			assertNotNull(ud5);
+			assertEquals(ud5.size(), 2);
+			logger.info("testUserViewGetAllinGroup");
+			logger.info(ud5.toString());
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			logger.error(EXCEPTION_CATCHED, e);
+			fail(EXCEPTION_CATCHED, e);
+		}
+	}
+
+	@Test
+	public void testUserViewGetAlltoAddInCompetition() {
+		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
+		List<UserDTO> ud6;
+		try {
+			ud6 = uvs.getAlltoAddInCompetition(1, 2);
+			assertNotNull(ud6);
+			assertEquals(ud6.size(), 2);
+			logger.info("testUserViewGetAlltoAddInCompetition");
+			logger.info(ud6.toString());
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			logger.error(EXCEPTION_CATCHED, e);
+			fail(EXCEPTION_CATCHED, e);
+		}
 	}
 
 }
