7d9adb84ce479474a4651461aa161a5dfd942346
Daniel <Daniel@192.168.0.102>
2016-07-16 10:45:29 +0300

    jdbc:postgresql

--- src/main/java/edu/softserveinc/healthbody/db/DataSourceRepository.java
+++ src/main/java/edu/softserveinc/healthbody/db/DataSourceRepository.java
@@ -23,7 +23,7 @@ public final class DataSourceRepository {
 	
 	public DataSource getPostgresJenkins()throws JDBCDriverException{
 		return new DataSource(DriverRepository.getInstance().getPostgresDriver(), 
-				"postgresql://127.10.182.2:5432/", "adminud8ggnm", " 6JxTBU-ab6KR");
+				"jdbc:postgresql://127.10.182.2:5432/jenkins", "adminud8ggnm", " 6JxTBU-ab6KR");
 	}
 	
 	public DataSource getPostgresLocalHost() throws JDBCDriverException {
