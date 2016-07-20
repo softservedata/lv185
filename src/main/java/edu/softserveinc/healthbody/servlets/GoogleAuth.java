869ba07281205427ac4b1cd5ab0eb394e55fbefb
volodya4u <volodya4u@gmail.com>
2016-07-20 11:40:42 +0300

    Rewrite GoogleAuth servlet

--- src/main/java/edu/softserveinc/healthbody/servlets/GoogleAuth.java
+++ src/main/java/edu/softserveinc/healthbody/servlets/GoogleAuth.java
@@ -19,79 +19,70 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class GoogleAuth extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public GoogleAuth() {
-        super();
-        
-    }
+	public GoogleAuth() {
+		super();
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-    	
-    	response.setContentType("application/json;charset=UTF-8");
-        
-        try {
-            // get code
-            String code = request.getParameter("code");
-            // format parameters to post
-            String urlParameters = "code="
-                    + code
-                    + "&client_id=48524677967-juniqolaio06efre3m3q7774097q50u8.apps.googleusercontent.com"
-                    + "&client_secret=KBpMscuWOZc43u-4KKpwbE5T"
-                    + "&redirect_uri=http://localhost:8080/lv185/GoogleAuth"
-                    + "&grant_type=authorization_code";
-            
-            //post parameters
-            URL url = new URL("https://accounts.google.com/o/oauth2/token");
-            URLConnection urlConn = url.openConnection();
-            urlConn.setDoOutput(true);
-            OutputStreamWriter writer = new OutputStreamWriter(
-                    urlConn.getOutputStream());
-            writer.write(urlParameters);
-            writer.flush();
-            
-            //get output in outputString 
-            String line, outputString = "";
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    urlConn.getInputStream()));
-            while ((line = reader.readLine()) != null) {
-                outputString += line;
-            }
-            
-            //get Access Token 
-            JsonObject json = (JsonObject)new JsonParser().parse(outputString);
-            String access_token = json.get("access_token").getAsString();
+	}
 
-            //get User Info 
-            url = new URL(
-                    "https://www.googleapis.com/oauth2/v1/userinfo?access_token="
-                            + access_token);
-            urlConn = url.openConnection();
-            outputString = "";
-            reader = new BufferedReader(new InputStreamReader(
-                    urlConn.getInputStream()));
-            while ((line = reader.readLine()) != null) {
-                outputString += line;
-            }
-            
-            // Convert JSON response into Pojo class
-            GooglePojo data = new Gson().fromJson(outputString, GooglePojo.class);
-            System.out.println(data);
-            writer.close();
-            reader.close();
-            
-        } catch (MalformedURLException e) {
-            System.out.println( e);
-        } catch (ProtocolException e) {
-            System.out.println( e);
-        } catch (IOException e) {
-            System.out.println( e);
-        }
-    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try {
+			// get code
+			String code = request.getParameter("code");
+			// format parameters to post
+			String urlParameters = "code=" + code
+					+ "&client_id=48524677967-juniqolaio06efre3m3q7774097q50u8.apps.googleusercontent.com"
+					+ "&client_secret=KBpMscuWOZc43u-4KKpwbE5T" + "&redirect_uri=http://localhost:8080/lv185/GoogleAuth"
+					+ "&grant_type=authorization_code";
+
+			// post parameters
+			URL url = new URL("https://accounts.google.com/o/oauth2/token");
+			URLConnection urlConn = url.openConnection();
+			urlConn.setDoOutput(true);
+			OutputStreamWriter writer = new OutputStreamWriter(urlConn.getOutputStream());
+			writer.write(urlParameters);
+			writer.flush();
+
+			// get output in outputString
+			String line, outputString = "";
+			BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+			while ((line = reader.readLine()) != null) {
+				outputString += line;
+			}
+
+			// get Access Token
+			JsonObject json = (JsonObject) new JsonParser().parse(outputString);
+			String access_token = json.get("access_token").getAsString();
+
+			// get User Info
+			url = new URL("https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + access_token);
+			urlConn = url.openConnection();
+			outputString = "";
+			reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+			while ((line = reader.readLine()) != null) {
+				outputString += line;
+			}
+
+			// Convert JSON response into Pojo class
+			GooglePojo data = new Gson().fromJson(outputString, GooglePojo.class);
+			System.out.println(data);
+			writer.close();
+			reader.close();
+
+		} catch (MalformedURLException e) {
+			System.out.println(e);
+		} catch (ProtocolException e) {
+			System.out.println(e);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
 
 }
